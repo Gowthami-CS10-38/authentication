53f17b578b0455d82f4b62f9338dc35311868b2d
Gowthami-CS10-38 <gnualap@gitam.in>
2023-01-20 16:48:36 +0530

    controllers and routes setup

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asynchandler(async(req,res)=>{
+  res.status(200).json({message:'Get goals'})
+})
+
+const setGoal = asynchandler(async(req,res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('please add text')
+    }
+   
+    res.status(200).json({message: 'set goal'})
+})
+
+const updateGoal = asynchandler(async(req,res)=>{
+    res.status(200).json({message: 'Update goal' + req.params.id})
+})
+
+const deleteGoal = asynchandler(async(req,res)=>{
+    res.status(200).json({message: 'Delete goal' + req.params.id})
+})
+
+module.exports={
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
